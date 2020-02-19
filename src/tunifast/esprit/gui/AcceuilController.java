1cd56c5172dfa1068b6998a78d1a0566f0f035d2
mohamed aziz haddad <med.Azizhaddad@gmail.Com>
2020-02-19 01:20:22 +0100

    template

--- src/tunifast/esprit/gui/AcceuilController.java
+++ src/tunifast/esprit/gui/AcceuilController.java
@@ -6,17 +6,23 @@
 package tunifast.esprit.gui;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -28,17 +34,19 @@ import javafx.stage.StageStyle;
 public class AcceuilController implements Initializable {
 
     @FXML
-    private JFXButton btnreserved;
+    private JFXHamburger hamburger;
+        @FXML
+    private JFXDrawer drawer;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        initDrawer();
         // TODO
     }    
 
-    @FXML
     private void anList(ActionEvent event) {
         loadWindow("pas/annonceList.fxml", "title");
     }
@@ -55,5 +63,31 @@ public class AcceuilController implements Initializable {
             Logger.getLogger(AcceuilController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    private void initDrawer() {
+ try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("toolbar.fxml"));
+            VBox toolbar = loader.load();
+            drawer.setSidePane(toolbar);
+          //  ToolbarController controller = loader.getController();
+        //    controller.setBookReturnCallback(this);
+        } catch (IOException ex) {
+            Logger.getLogger(AcceuilController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        HamburgerSlideCloseTransition task = new HamburgerSlideCloseTransition(hamburger);
+        task.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_CLICKED, (Event event) -> {
+            drawer.toggle();
+        });
+        drawer.setOnDrawerOpening((event) -> {
+            task.setRate(task.getRate() * -1);
+            task.play();
+            drawer.toFront();
+        });
+        drawer.setOnDrawerClosed((event) -> {
+            drawer.toBack();
+            task.setRate(task.getRate() * -1);
+            task.play();
+        });    }
     
 }
--- src/tunifast/esprit/gui/ToolbarController.java
+++ src/tunifast/esprit/gui/ToolbarController.java
@@ -5,9 +5,21 @@
  */
 package tunifast.esprit.gui;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import tunifast.esprit.Utils.TuniFastUtil;
 
 /**
  * FXML Controller class
@@ -16,12 +28,21 @@ import javafx.fxml.Initializable;
  */
 public class ToolbarController implements Initializable {
 
+    @FXML
+    private JFXButton btnreserved;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void anList(ActionEvent event) {
+        TuniFastUtil.loadWindow(getClass().getResource("pas/annonceList.fxml"), "reserver" , null);
+    }
+
+ 
 }
